d6274c8022933a45b5d2614392c06f362a6c2c6b
Tanțoș Alexandra-Bianca <114863064+AlexandraTantos@users.noreply.github.com>
2023-09-30 12:17:30 +0300

    Add files via upload

---
+++ Comfy Store/src/pages/about.js
@@ -0,0 +1,3 @@
+import '../toggleSidebar.js';
+import '../cart/toggleCart.js';
+import '../cart/setupCart.js';
---
+++ Comfy Store/src/pages/products.js
@@ -0,0 +1,28 @@
+import '../toggleSidebar.js';
+import '../cart/toggleCart.js';
+import '../cart/setupCart.js';
+
+import setupSearch from '../filters/search.js';
+import setupCompanies from '../filters/companies.js';
+import setupPrice from '../filters/price.js';
+
+import { store, setupStore } from '../store.js';
+import display from '../displayProducts.js';
+import { getElement } from '../utils.js';
+import fetchProducts from '../fetchProducts.js';
+
+const init = async () => {
+  const loading = getElement('.page-loading');
+  if (store.length < 1) {
+    const products = await fetchProducts();
+    setupStore(products);
+  }
+  display(store, getElement('.products-container'));
+
+  setupSearch(store);
+  setupCompanies(store);
+  setupPrice(store);
+  loading.style.display = 'none';
+};
+
+init();
